8e02f71622050dcbacb044da0d497a4a88d0df57
Doug Keller <dkeller@synergydatasystems.com>
2016-11-17 01:44:37 -0500

    Add gulp task for combining scripts

---
+++ TermProject/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var vendorPath = './public/bower_components/';
+var scriptPath = './public/javascripts/';
+
+gulp.task('generateScripts', function() {
+  var scriptDependencies = [
+    vendorPath + 'angular/angular.js',
+    scriptPath + 'angular_modules.js',
+    scriptPath + '*.js',
+    scriptPath + '**/*.js'
+  ];
+
+  return gulp.src(scriptDependencies)
+             .pipe(concat('client_app.js'))
+             .pipe(uglify())
+             .pipe(gulp.dest('./public'));
+});
